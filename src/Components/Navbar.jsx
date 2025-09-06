a93e3dab577912f20c31ec654ff038629734b8c0
gresaudiapasaribu <gresaudipasaribu@gmail.com>
2025-09-06 19:08:31 +0700

    Add navbar title in feature/navbar branch

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
       <div className="flex justify-between items-center">
         <img className="filter saturate-200" src={logo} alt="" />
         <img className="p-2 bg-white/15 rounded-md" src={sunIcon} alt="" />
+        <h1>Hello from Navbar 🚀</h1>
+
       </div>
     </div>
   );
